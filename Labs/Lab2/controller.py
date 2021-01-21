bb80612868ba18bf911407aa7560526c38a9b706
JackShih <pyopoly@gmail.com>
2021-01-21 13:28:35 -0800

    in controller.py simulate(self, seconds) method halfway complete. Working on running the code on each second.

--- Labs/Lab2/controller.py
+++ Labs/Lab2/controller.py
@@ -3,7 +3,8 @@ import datetime
 import math
 import asteroid
 import vector
-
+import time
+import datetime
 
 class Controller:
 
@@ -18,9 +19,32 @@ class Controller:
             self._list_of_asteroids.append(new_asteroid)
 
     def simulate(self, seconds):
-        for x in range(seconds):
-            for y in self._list_of_asteroids:
-                y.move()
+
+        index = 0
+        while seconds > 0 and (time.time() %1) == 0:
+            # time.sleep(1-(time.time() % 1))
+            print(datetime.datetime.fromtimestamp(time.time()))
+            self._list_of_asteroids[index].move()
+            # time.sleep(1)
+            print(self._list_of_asteroids[index]._asteroid_ID)
+            seconds -= 1
+            index += 1
+            if index == len(self._list_of_asteroids):
+                index = 0
+
 
     def __calculate_circumference(self, radius):
         return 2 * math.pi * radius
+ccc = Controller(10)
+ccc.simulate(25)
+
+start_time = time.time()
+start_time = datetime.datetime.fromtimestamp(start_time)
+
+print("Simulation start time: {0}".format(start_time))
+delta = datetime.datetime(start_time.year, start_time.month, start_time.day, start_time.hour,
+                          start_time.minute, start_time.second)
+delta = start_time - delta
+print(datetime.datetime.timestamp(start_time))
+print(time.time())
+print(1- (time.time() % 1))
