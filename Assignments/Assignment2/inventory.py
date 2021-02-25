15788da63bedec64fd642510ac83315c4ff6a14d
JackShih <pyopoly@gmail.com>
2021-02-24 18:31:09 -0800

    Initial commit. created backbone classes

---
+++ Assignments/Assignment2/inventory.py
@@ -0,0 +1,48 @@
+
+
+class Toy:
+    pass
+
+
+class StuffedAnimal:
+    pass
+
+
+class Candy:
+    pass
+
+
+class SantaWorkshop(Toy):
+    pass
+
+
+class RCSpider(Toy):
+    pass
+
+
+class RobotBunny(Toy):
+    pass
+
+
+class DancingSkeleton(StuffedAnimal):
+    pass
+
+
+class Reindeer(StuffedAnimal):
+    pass
+
+
+class EasterBunny(StuffedAnimal):
+    pass
+
+
+class PumpkinCaramelToffee(Candy):
+    pass
+
+
+class CandyCane(Candy):
+    pass
+
+
+class CremeEgg(Candy):
+    pass
